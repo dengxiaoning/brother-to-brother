2ce33af783e015e88705ef80f3384f9b6125428c
dex <412693453@qq.com>
2019-11-17 11:37:31 +0800

    first of commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <header-component></header-component>
+    <div class="content">
+      <div class="left-menu">
+        <h5>自定义颜色</h5>
+        <el-menu
+          default-active="2"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b">
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>导航一</span>
+            </template>
+            <el-menu-item-group>
+              <template slot="title">分组一</template>
+              <el-menu-item index="1-1">选项1</el-menu-item>
+              <el-menu-item index="1-2">选项2</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="分组2">
+              <el-menu-item index="1-3">选项3</el-menu-item>
+            </el-menu-item-group>
+            <el-submenu index="1-4">
+              <template slot="title">选项4</template>
+              <el-menu-item index="1-4-1">选项1</el-menu-item>
+            </el-submenu>
+          </el-submenu>
+          <el-menu-item index="2">
+            <i class="el-icon-menu"></i>
+            <span slot="title">导航二</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <i class="el-icon-document"></i>
+            <span slot="title">导航三</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <i class="el-icon-setting"></i>
+            <span slot="title">导航四</span>
+          </el-menu-item>
+        </el-menu>
+      </div>
+      <div class="main-txt"></div>
+    </div>
+    <edit-component></edit-component>
   </div>
 </template>
 
 <script>
+  import HeaderComponent from "@/components/header_component";
+  import EditComponent from "@/components/edit_component";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+    methods: {
+    },
+  components:{
+      HeaderComponent,
+      EditComponent
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.left-menu{
+  width: 230px;
+  height:calc(100% - 40px);
 }
 </style>
---
+++ src/components/header_component.vue
@@ -0,0 +1,112 @@
+<template>
+    <div class="nav-top">
+        <div class="go-back" >
+            <i class="el-icon-arrow-left icon-back"></i>
+        </div>
+        <div class="map-name" >{{name}}</div>
+        <div class="icon-style">
+            <i class="el-icon-edit icon-size"  @click="showEditDialog"></i>
+        </div>
+    </div>
+</template>
+
+<script type="text/ecmascript-6">
+    import BToB from './b_to_b.js';
+
+    export default {
+        name: "header_component",
+        data(){
+          return{
+              name:'春节联欢晚会'
+          }
+        },
+        created(){
+            this.listenBtoBFromEdit();
+        },
+        methods:{
+            showEditDialog(){
+                BToB.$emit("switch_dialog",true);
+            },
+            listenBtoBFromEdit(){
+                BToB.$on("update_template_params",val =>{
+                    this.name = val;
+                })
+            },
+
+        }
+    }
+</script>
+
+<style scoped  lang="less" >
+    .nav-top {
+        position: fixed;
+        top: 0;
+        left: 0;
+        height: 40px;
+        line-height: 40px;
+        width: 100%;
+        background: rgba(28, 30, 46, 1);
+        border-bottom: 1px solid #fff;
+        box-sizing: border-box;
+        z-index: 99;
+    }
+    .go-back {
+        position: absolute;
+        left: 0;
+        top: 0;
+        z-index: 11;
+        text-align: center;
+        line-height: 39px;
+        width: 40px;
+        height: 40px;
+        background: linear-gradient(
+                17deg,
+                rgba(56, 77, 218, 1) 0%,
+                rgba(40, 132, 209, 1) 100%
+        );
+        box-shadow: 0px 4px 10px 0px rgba(188, 205, 232, 0.1);
+    }
+    .map-name {
+        float: left;
+        color: #fff;
+        font-size: 14px;
+        margin-left: 57px;
+    }
+    .map-name-text {
+        float: left;
+        color: #000;
+        background: #fff;
+        height: 24px;
+        line-height: 24px;
+        width: 142px;
+        margin-left: 57px;
+        border: 0;
+        outline: none;
+        margin-top: 8px;
+        font-size: 12px;
+        box-sizing: border-box;
+        padding: 0 5px;
+    }
+    .icon-style {
+        float: left;
+        color: #676976;
+        font-size: 18px;
+        margin-left: 12px;
+        vertical-align: middle;
+    i:hover {
+        color: #2e74d4;
+    }
+    }
+    .icon-size {
+        font-size: 16px;
+        cursor: pointer;
+        color: #ffffff;
+    }
+    .icon-back {
+        font-size: 20px;
+        color: #fff;
+    }
+    .icon-left {
+        margin-left: 10px;
+    }
+</style>
